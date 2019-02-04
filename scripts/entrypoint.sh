98027e2d961f0154a86b753f6de16ea9c0792bea
Daniel Agar <daniel@agar.ca>
2019-02-03 20:55:40 -0500

    fix entrypoint.sh usage

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,16 +1,26 @@
 #!/bin/bash
 
+# Start virtual X server in the background if there is no display
+if [[ -x "$(command -v Xvfb)" && ! -z "$DISPLAY" ]]; then
+	echo "Starting Xvfb"
+	Xvfb :99 -screen 0 1600x1200x24+32 &
+	export DISPLAY=:99
+fi
+
 # Add local user
 # Either use the LOCAL_USER_ID if passed in at runtime or
 # fallback
 
-USER_ID=${LOCAL_USER_ID:-9001}
+if [ -n "${LOCAL_USER_ID}" ]; then
 
-echo "Starting with UID : $USER_ID"
-useradd --shell /bin/bash -u $USER_ID -o -c "" -m user
-usermod -a -G dialout user
-export HOME=/home/user
-mkdir -p $HOME/.ccache
-chown -R $USER_ID $HOME/.ccache
+	echo "Starting with UID : $LOCAL_USER_ID"
 
-exec gosu user "$@"
+	# modify existing user's id
+	usermod -u $LOCAL_USER_ID user
+
+	exec gosu user "$@"
+
+else
+
+	exec "$@"
+fi
